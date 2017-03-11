441c8dac3ef4a0c426c82e74d38a404040b1e543
Vishesh Banga <visheshbanga@users.noreply.github.com>
2017-03-11 18:18:49 +0530

    Add files via upload

---
+++ pg_config.sh
@@ -0,0 +1,10 @@
+sudo apt-get -qqy update
+sudo apt-get -qqy install postgresql python-psycopg2
+sudo apt-get -qqy install python-sqlalchemy
+sudo apt-get -qqy install python-pip
+sudo pip install werkzeug==0.8.3
+sudo pip install flask==0.9
+sudo pip install Flask-Login==0.1.3
+sudo pip install oauth2client
+sudo pip install requests
+sudo pip install httplib2
